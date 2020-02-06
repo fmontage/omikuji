7dfd29c3a2cf701abc8a10d0c7d2e9167d1d9cc6
fmontage <fmontage@outlook.jp>
2020-02-06 17:03:16 +0900

    末吉、小吉を追加、出る確率を変更

--- js/main.js
+++ js/main.js
@@ -4,8 +4,20 @@
     const btn = document.getElementById('btn');
     
     btn.addEventListener('click', () => {
-        const results = ['大吉', '中吉', '凶', '末吉'];
-        btn.textContent = results[Math.floor(Math.random() * results.length)];
+
+        const n = Math.random();
+        if (n < 0.05) {
+            btn.textContent = '凶';
+        } else if (n < 0.2) {
+            btn.textContent = '大吉';
+        } else if (n < 0.5) {
+            btn.textContent = '中吉';
+        } else if (n < 0.75) {
+            btn.textContent = '末吉';
+        } else {
+            btn.textContent = '小吉';
+        }
+        
 
     });
 }
